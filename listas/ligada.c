927db318fe50cc798ab6c83f46549b2d031db08a
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-04-24 23:09:12 -0500

    Recorrer, insertar inicio, fin y sha :fire:

--- listas/ligada.c
+++ listas/ligada.c
@@ -1,2 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ligada.h"
+
+void init(Ligada *lista) {
+  lista->primero = NULL;
+}
+
+void recorrer(Ligada *lista) {
+  //vacia
+  recursivo(lista->primero);
+}
+
+void recursivo(Nodo *nodo) {
+  if(nodo->siguiente != NULL){
+    printf("%d\n", nodo->dato);
+    recursivo(nodo->siguiente);
+  }
+}
+
+void insertar_inicio(Ligada *lista, int valor) {
+  Nodo *temporal = NULL;
+  temporal = (Nodo *) malloc(sizeof(Nodo));
+  if(temporal == NULL)
+    return;
+
+  temporal->dato = valor;
+  temporal->siguiente = lista->primero;
+  lista->primero = temporal;
+}
+
+void insertar_final(Ligada *lista, int valor) {
+  //vacia
+  Nodo *final = lista->primero;
+  while(final->siguiente != NULL) {
+    final = final->siguiente;
+  }
+  // Se puede hacer sin usar el temporal con solo final->siguiente en su lugar
+  Nodo *temporal = NULL;
+  temporal = (Nodo *) malloc(sizeof(Nodo));
+  if (temporal == NULL)
+    return;
+
+  temporal->dato = valor;
+  temporal->siguiente = NULL;
+  final->siguiente = temporal;
+}
--- listas/ligada.h
+++ listas/ligada.h
@@ -1,2 +1,21 @@
 #ifndef __LIGADA_H_
 #define __LIGADA_H_
+
+struct Nodos {
+  int dato;
+  struct Nodos *siguiente;
+};
+
+typedef struct Nodos Nodo;
+
+typedef struct Ligadas {
+  Nodo *primero;
+} Ligada;
+
+void init(Ligada *);
+void recorrer(Ligada *);
+void recursivo(Nodo *);
+void insertar_inicio(Ligada *, int);
+void insertar_final(Ligada *lista, int valor);
+
+#endif
